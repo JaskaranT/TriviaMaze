b27407eea35f4ebb50ae3f05f1bdebffc4055b73
Michael Castro <mdc12@uw.edu>
2023-11-05 20:20:25 -0800

    DataBase class to connect to database file and fetch data (questions and answers)

---
+++ model/DatabaseQ.java
@@ -0,0 +1,44 @@
+package model;
+
+import org.sqlite.SQLiteDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseQ {
+
+  public static Question_Answer getQuestion() {
+    SQLiteDataSource ds = null;
+
+    //establish connection (creates db file if it does not exist :-)
+    try {
+      ds = new SQLiteDataSource();
+      ds.setUrl("jdbc:sqlite:TriviaMazeQuestions.db");
+    } catch ( Exception e ) {
+      e.printStackTrace();
+      System.exit(0);
+    }
+
+    String query = "SELECT * FROM questions ORDER BY RANDOM() Limit 1";
+    try ( Connection conn = ds.getConnection();
+          Statement stmt = conn.createStatement(); ) {
+
+      ResultSet rs = stmt.executeQuery(query);
+
+      //walk through a random 'row' of results, grab data by column/field name
+      if ( rs.next() ) {
+        String question = rs.getString( "QUESTION" );
+        String answer = rs.getString( "ANSWER" );
+
+        return new Question_Answer(question, answer);
+
+      }
+    } catch ( SQLException e ) {
+      e.printStackTrace();
+      System.exit( 0 );
+    }
+    return null;
+  }
+}
