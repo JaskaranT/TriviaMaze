9f852e99aa833e274787631608ef60d6ae821bf5
Michael Castro <mdc12@uw.edu>
2023-11-05 20:26:33 -0800

    Wrote enough code for Door, Question Answer class to be able to create objects of themselves.

--- model/Door.java
+++ model/Door.java
@@ -4,21 +4,30 @@ public class Door {
 
     private boolean myIsLocked;
 
+    private Question_Answer myQuestion;
 
 
-    public Door(){
 
+    public Door() {
+        myIsLocked = true;
+        myQuestion = DatabaseQ.getQuestion();
     }
 
     public void unlock() {
-
+        myIsLocked = false;
     }
 
     public void lock() {
-
+        myIsLocked = true;
     }
 
+    public String getQuestion() {
+        return myQuestion.getQuestionText();
+    }
 
+    public String getAnswer() {
+        return myQuestion.getCorrectAnswer();
+    }
 
 
 }
--- model/Question_Answer.java
+++ model/Question_Answer.java
@@ -6,12 +6,18 @@ public class Question_Answer {
 
     private String myAnswer;
 
+    public Question_Answer(String theQuestion, String theAnswer) {
+        myQuestion = theQuestion;
+        myAnswer = theAnswer;
+    }
+
 
     public String getQuestionText() {
-
+        return myQuestion;
     }
 
     public String getCorrectAnswer() {
+        return myAnswer;
 
     }
 }
