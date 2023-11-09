ab6bb97515b8d2e1b0749e2f3c0aa6ca53e58889
Michael Castro <mdc12@uw.edu>
2023-11-08 22:45:59 -0800

    Wrote some code for our display class to get an idea on what will need to display to the user.

--- view/Display.java
+++ view/Display.java
@@ -1,4 +1,55 @@
 package view;
 
+import model.TriviaMaze;
+
 public class Display {
+  private TriviaMaze myMaze;
+  public Display(TriviaMaze theMaze) {
+    myMaze = theMaze;
+  }
+
+  public void displayMaze() {
+    System.out.print(myMaze.toString);
+  }
+
+  public void displayRoom () {
+    System.out.print(myMaze.getRoomLocation().toString());
+  }
+  public void StartIntro() {
+    System.out.println("WELCOME TO TRIVIA MAZE");
+    GameInfo();
+  }
+  public void GameInfo () {
+    System.out.println("In this game you will Travel through rooms answering questions where you main goal" +
+            "will be to reach the exit");
+    System.out.println("Everytime you get a correct answer, that room will remain unlock but be careful because one " +
+            "wrong answer will lock that room forever.");
+  }
+
+  public void displayInstruction() {
+    //instructions are yet to be decided.
+  }
+  public void displayQuestion(String theQuestion) {
+    System.out.print(theQuestion);
+  }
+
+  public void displayAnswer(String theAnswer) {
+    System.out.print(theAnswer);
+  }
+
+  public void displayPlayerWon() {
+    System.out.println("CONGRATULATIONS! YOU WON!");
+  }
+
+  public void displayPlayerLost() {
+    System.out.println("Sorry, there are no more doors to unlock. You Have Lost...");
+  }
+
+  public void displayFileMenu() {
+
+  }
+  public void displayHelpMenu() {
+
+  }
+
 }
