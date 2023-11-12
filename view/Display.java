346b297299c9fa32cc5199de190a11a7d255cb5e
Michael <mdc12@uw.edu>
2023-11-12 13:45:45 -0800

    Added some new display methods do display if answer is current or not and completed the instructions method. Might still need more work thoughtout.

--- view/Display.java
+++ view/Display.java
@@ -20,14 +20,22 @@ public class Display {
     GameInfo();
   }
   public void GameInfo () {
-    System.out.println("In this game you will Travel through rooms answering questions where you main goal" +
-            "will be to reach the exit");
-    System.out.println("Everytime you get a correct answer, that room will remain unlock but be careful because one " +
-            "wrong answer will lock that room forever.");
+    System.out.println("In this game you will Travel through rooms answering questions where you main goal");
+    System.out.println("will be to reach the exit");
+    System.out.println("Everytime you get a correct answer, that room will remain unlock but be careful because");
+    System.out.println( "one wrong answer will lock that room forever.");
   }
 
   public void displayInstruction() {
-    //instructions are yet to be decided.
+    System.out.println("Instructions: ");
+    System.out.println("In this game you will be placed in the start location.");
+    System.out.println("You will then need to choose room that's adjacent to we you are currently at.");
+    System.out.println("There are four options for moving. NORTH, WEST, EAST, SOUTH");
+    System.out.println("You will type one and will approach the door as long as its not a wall,");
+    System.out.println(" or locked forever door. Once you approach it you will be prompt to answer a question.");
+    System.out.println("If you answer is correct you will proceed to the room, if its incorrect, the door will lock");
+    System.out.println(" forever meaning you can go through there anymore so you will need to find another way.");
+    System.out.println("if you lock yourself and there's no where to go then its game over.");
   }
   public void displayQuestion(String theQuestion) {
     System.out.print(theQuestion);
@@ -45,6 +53,14 @@ public class Display {
     System.out.println("Sorry, there are no more doors to unlock. You Have Lost...");
   }
 
+  public void displayCorrect() {
+    System.out.println("CORRECT! You may proceed");
+  }
+
+  public void displayIncorrect() {
+    System.out.println("INCORRECT! Door is now locked forever");
+  }
+
   public void displayFileMenu() {
     System.out.println("Type one of the following:");
     System.out.println("\"Save\" to save game");
@@ -52,6 +68,7 @@ public class Display {
     System.out.println("\"Exit\" to exit game");
   }
   public void displayHelpMenu() {
+    System.out.println("Type one of the following:");
     System.out.println("\"About\" for information about the game");
     System.out.println("\"Instr\" for game Instructions");
   }
