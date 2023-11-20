7f80a39104a472a64c30e2a3775ed8b3b7ce1521
Michael Castro <mdc12@uw.edu>
2023-11-19 23:25:07 -0800

    set fields to private, added method to help check if player has visited room. Added methods that checks if player can move and method that moves player

--- model/TriviaMaze.java
+++ model/TriviaMaze.java
@@ -5,9 +5,9 @@ public class TriviaMaze {
 
     private int myY;
 
-    private Room[][] myRooms;
+    private final Room[][] myRooms;
 
-    private int size = 4;
+    private final int size = 4;
 
 
     public TriviaMaze() {
@@ -17,7 +17,7 @@ public class TriviaMaze {
         generateMaze();
     }
 
-    public void generateMaze() {
+    private void generateMaze() {
         for (int x = 0; x < myRooms.length; x++) {
             for (int y = 0; y < myRooms[x].length; y++) {
                 myRooms[x][y] = new Room(x, y);
@@ -29,17 +29,48 @@ public class TriviaMaze {
         return myX == size - 1 && myY == size -1;
     }
 
+
     public Room getRoomLocation() {
         return myRooms[myX][myY];
     }
+
+    public boolean checkVisited(String theDirection) {
+        int row = myX;
+        int col = myY;
+        switch (theDirection) {
+            case "north" -> row--;
+            case "west" -> col--;
+            case "south" -> row++;
+            case "east" -> col++;
+        }
+        return myRooms[row][col].visited();
+    }
+
+    public boolean canMove(int theDirection) {
+        Door current = getRoomLocation().getDoor(theDirection);
+        return current != null && !current.isLockedForever();
+    }
+
+    public void MovePlayer (String theDirection) {
+        switch (theDirection) {
+            case "north" -> myX--;
+            case "west" -> myY--;
+            case "south" -> myX++;
+            case "east" -> myY++;
+        }
+    }
+
 
     public String toString() {
         StringBuilder triviaMaze = new StringBuilder();
         for (int i = 0; i < myRooms.length; i++) {
             triviaMaze.append("\n");
             for (int j = 0; j < myRooms[0].length; j++) {
-                if (i == 0 && j == 0 ) {
+                if ((i == 0 && j == 0) && !(myX == i && myY == j)) {
                     triviaMaze.append("|ST|");
+                }
+                else if (myX == i && myY == j) {
+                    triviaMaze.append("|PL|");
                 }
                 else if (i == myRooms.length - 1 && j == myRooms[0].length - 1) {
                     triviaMaze.append("|ED|");
