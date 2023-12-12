5d6b51bce5492ac3a7dae96b47d9eed52230bb3b
Michael Castro <mdc12@uw.edu>
2023-12-12 01:18:33 -0800

    main method to run game

---
+++ controller/Main.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.awt.*;
+
+public class Main {
+
+    private Main() {
+
+    }
+
+    public static void main (String... theArgs) {
+        EventQueue.invokeLater(GameController::new);
+    }
+}
