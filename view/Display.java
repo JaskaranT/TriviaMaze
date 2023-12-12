11d5cac39f77373c805f3dacef5caf922a3193f9
Michael Castro <mdc12@uw.edu>
2023-12-12 01:15:20 -0800

    Added methods to display game cleaner.

--- view/Display.java
+++ view/Display.java
@@ -9,39 +9,87 @@ public class Display {
   }
 
   public void displayMaze() {
+    System.out.println("\t\t\t  MAZE");
+    System.out.print("\t\t________________");
     System.out.print(myMaze.toString());
+    System.out.println("\t\t----------------");
+    System.out.println("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\");
   }
 
   public void displayRoom () {
     System.out.print(myMaze.getRoomDisplay());
+  }
+
+  public void DisplayGameType() {
+    System.out.println("""
+
+
+            NEW - 𝐏𝐥𝐚𝐲 𝐍𝐞𝐰 𝐆𝐚𝐦𝐞                   LOAD - 𝐋𝐨𝐚𝐝 𝐆𝐚𝐦𝐞""");
+
+  }
+
+  public void displayTitle() {
+    System.out.println("\n" +
+
+            "\n" +
+            "──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────\n" +
+            "─██████████████─████████████████───██████████─██████──██████─██████████─██████████████────██████──────────██████─██████████████─██████████████████─██████████████─\n" +
+            "─██░░░░░░░░░░██─██░░░░░░░░░░░░██───██░░░░░░██─██░░██──██░░██─██░░░░░░██─██░░░░░░░░░░██────██░░██████████████░░██─██░░░░░░░░░░██─██░░░░░░░░░░░░░░██─██░░░░░░░░░░██─\n" +
+            "─██████░░██████─██░░████████░░██───████░░████─██░░██──██░░██─████░░████─██░░██████░░██────██░░░░░░░░░░░░░░░░░░██─██░░██████░░██─████████████░░░░██─██░░██████████─\n" +
+            "─────██░░██─────██░░██────██░░██─────██░░██───██░░██──██░░██───██░░██───██░░██──██░░██────██░░██████░░██████░░██─██░░██──██░░██─────────████░░████─██░░██─────────\n" +
+            "─────██░░██─────██░░████████░░██─────██░░██───██░░██──██░░██───██░░██───██░░██████░░██────██░░██──██░░██──██░░██─██░░██████░░██───────████░░████───██░░██████████─\n" +
+            "─────██░░██─────██░░░░░░░░░░░░██─────██░░██───██░░██──██░░██───██░░██───██░░░░░░░░░░██────██░░██──██░░██──██░░██─██░░░░░░░░░░██─────████░░████─────██░░░░░░░░░░██─\n" +
+            "─────██░░██─────██░░██████░░████─────██░░██───██░░██──██░░██───██░░██───██░░██████░░██────██░░██──██████──██░░██─██░░██████░░██───████░░████───────██░░██████████─\n" +
+            "─────██░░██─────██░░██──██░░██───────██░░██───██░░░░██░░░░██───██░░██───██░░██──██░░██────██░░██──────────██░░██─██░░██──██░░██─████░░████─────────██░░██─────────\n" +
+            "─────██░░██─────██░░██──██░░██████─████░░████─████░░░░░░████─████░░████─██░░██──██░░██────██░░██──────────██░░██─██░░██──██░░██─██░░░░████████████─██░░██████████─\n" +
+            "─────██░░██─────██░░██──██░░░░░░██─██░░░░░░██───████░░████───██░░░░░░██─██░░██──██░░██────██░░██──────────██░░██─██░░██──██░░██─██░░░░░░░░░░░░░░██─██░░░░░░░░░░██─\n" +
+            "─────██████─────██████──██████████─██████████─────██████─────██████████─██████──██████────██████──────────██████─██████──██████─██████████████████─██████████████─\n" +
+            "──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────");
   }
   public void StartIntro() {
     System.out.println("WELCOME TO TRIVIA MAZE");
     GameInfo();
   }
   public void GameInfo () {
-    System.out.println("In this game you will Travel through rooms answering questions where you main goal");
-    System.out.println("will be to reach the exit");
-    System.out.println("Everytime you get a correct answer, that room will remain unlock but be careful because");
-    System.out.println( "one wrong answer will lock that room forever.");
+    System.out.println("In this game you will be placed in the start location.");
+    System.out.println("You will then need to choose room that's adjacent to we you are currently at.");
+    System.out.println("There are four options for moving. NORTH, WEST, EAST, SOUTH.");
+    System.out.println("You will type one and will approach the door as long as its not a wall,");
+    System.out.println("or locked forever door. Once you approach it you will be prompt to answer a question.");
+    System.out.println("If you answer is correct you will proceed to the room, if its incorrect, the door will lock");
+    System.out.println("forever meaning you can't go through there anymore so you will need to find another way.");
+    System.out.println("If you lock yourself out and there's no where to go then its game over.");
   }
 
   public void displayInstruction() {
     System.out.println("Instructions: ");
-    System.out.println("In this game you will be placed in the start location.");
-    System.out.println("You will then need to choose room that's adjacent to we you are currently at.");
-    System.out.println("There are four options for moving. NORTH, WEST, EAST, SOUTH");
-    System.out.println("You will type one and will approach the door as long as its not a wall,");
-    System.out.println(" or locked forever door. Once you approach it you will be prompt to answer a question.");
-    System.out.println("If you answer is correct you will proceed to the room, if its incorrect, the door will lock");
-    System.out.println(" forever meaning you can go through there anymore so you will need to find another way.");
-    System.out.println("if you lock yourself and there's no where to go then its game over.");
+    System.out.println("To move to a different room, you can type:");
+    System.out.println("\"North\" to move up");
+    System.out.println("\"South\" to move down");
+    System.out.println("\"East\" to move right");
+    System.out.println("\"West\" to move left");
+    System.out.println();
+    System.out.println("In the MAZE display:");
+    System.out.println("|ST| means the room where you started");
+    System.out.println("|RM| means a room");
+    System.out.println("|ED| means the end room to win");
+    System.out.println();
+    System.out.println("In the Room display:");
+    System.out.println("|XX| means you can't go through either because it's a wall or door is locked forever");
+    System.out.println("|OP| means you can access that door since it was unlocked");
+    System.out.println("|LK| means the door is locked but is accessible if Question is answered correctly");
+    System.out.println();
+    System.out.println("There are three types of questions:");
+    System.out.println("Short Answer - You must type the answer");
+    System.out.println("Multiple Choice - You must type either A, B, or C");
+    System.out.println("True or False - You must type either True or False");
+    System.out.println();
   }
-  public void displayQuestion(String theQuestion) {
+  public void displayQuestion(final String theQuestion) {
     System.out.println(theQuestion);
   }
 
-  public void displayAnswer(String theAnswer) {
+  public void displayAnswer(final String theAnswer) {
     System.out.println("The correct answer was " + theAnswer);
   }
 
@@ -62,8 +110,9 @@ public class Display {
   }
 
   public void displayDirection() {
-    System.out.println("|NORTH || WEST || SOUTH|| EAST |");
-    System.out.println("Type a direction.");
+    System.out.println("Options: |MENU| - |HELP| ");
+    System.out.println("Type an option or direction");
+    System.out.println();
   }
 
   public void displayWrongIn() {
