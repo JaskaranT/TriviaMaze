7ef53d71a652a96e168e9a68fd2f1458484c9557
Jaskaran Toor <kssjay23@gmail.com>
2023-12-15 18:50:45 -0800

    javadoc, naming conventions complete.

--- model/TriviaMaze.java
+++ model/TriviaMaze.java
@@ -2,18 +2,41 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * @author Michael Castro
+ * @author Jaskaran Toor
+ * @author Tu Phan
+ * @version 12/12/2023
+ *
+ * This is the trivia maze class that holds all logic for the maze.
+ */
+
 public class TriviaMaze implements Serializable {
+    /**
+     * Players x - coordinate.
+     */
     private int myX;
-
+    /**
+     * Players y - coordinate.
+     */
     private int myY;
-
+    /**
+     * 2d array of rooms for the maze.
+     */
     private final Room[][] myRooms;
-
+    /**
+     * Constant for the size of the maze.
+     */
     private static final int MY_SIZE = 4;
-
+    /**
+     * Door that holds information on current door.
+     */
     private Door myCurrentDoor;
 
-
+    /**
+     * Constructor that builds the maze and
+     * puts player at top left of maze.
+     */
     public TriviaMaze() {
         myX = 0;
         myY = 0;
@@ -21,6 +44,9 @@ public class TriviaMaze implements Serializable {
         generateMaze();
     }
 
+    /**
+     * Generates the size and layout of the maze.
+     */
     private void generateMaze() {
 
         for (int x = 0; x < myRooms.length; x++) {
@@ -40,46 +66,92 @@ public class TriviaMaze implements Serializable {
         }
     }
 
+    /**
+     * Checks if user is at the end of the maze.
+     * @return user location.
+     */
     public boolean isGameWon() {
         return myX == MY_SIZE - 1 && myY == MY_SIZE - 1;
     }
 
+    /**
+     * Sets the current door the player is at.
+     * @param theDirection
+     */
     public void setCurrentDoor(final String theDirection) {
         myCurrentDoor = myRooms[myX][myY].getDoor(theDirection);
     }
 
+    /**
+     * checks if user can move to the desired direction.
+     * @param theDoor
+     * @return boolean theDoor
+     */
     private boolean canMove(final Door theDoor) {
         return theDoor != null && !theDoor.isLockedForever();
     }
+
+    /**
+     * checks if user can move in the desired direction
+     * @return boolean myCurrentDoor
+     */
     public boolean canMove() {
         return myCurrentDoor != null && !myCurrentDoor.isLockedForever();
     }
 
+    /**
+     * Checks if current door is locked
+     * @return boolean
+     */
     public boolean checkLocked() {
         return myCurrentDoor.isLocked();
     }
 
+    /**
+     * Checks if current door is locked forever.
+     * @return boolean
+     */
     public boolean checkLockedForever() {
         return myCurrentDoor.isLockedForever();
     }
 
+    /**
+     * Gets the question at the current door.
+     * @return String quesiton.
+     */
     public String getDoorQuestion() {
         return myCurrentDoor.getQuestion();
     }
 
+    /**
+     * Gets the answer at the current door.
+     * @return String answer
+     */
     public String getDoorAnswer() {
         return myCurrentDoor.getAnswer();
     }
 
+    /**
+     * Compares the users answer with the correct answer
+     * @param theAnswer
+     */
     public void checkPlayerAnswer(String theAnswer) {
         myCurrentDoor.answer(theAnswer);
     }
 
+    /**
+     * Displays the room and all 4 doors in the room.
+     * @return String
+     */
     public String getRoomDisplay() {
         return myRooms[myX][myY].toString();
     }
 
-
+    /**
+     * Moves the players x and y coordinates based on the users
+     * input.
+     * @param theDirection
+     */
     public void MovePlayer (final String theDirection) {
         switch (theDirection) {
             case "north" -> myX--;
@@ -89,6 +161,11 @@ public class TriviaMaze implements Serializable {
         }
     }
 
+    /**
+     * Checks if there is a possible route remaining
+     * to the end of the maze.
+     * @return boolean
+     */
     public boolean possibleRoute() {
         //clear all visited rooms
         for (Room[] rooms : myRooms) {
@@ -99,6 +176,14 @@ public class TriviaMaze implements Serializable {
         return move(myX, myY);
 
     }
+
+    /**
+     * Recursive method that takes players coordinates and
+     * checks if the players desired direction is valid.
+     * @param theX
+     * @param theY
+     * @return boolean
+     */
     private boolean move(final int theX, final int theY) {
         boolean success = false;
         if (validMove(theX, theY)) {
@@ -121,14 +206,31 @@ public class TriviaMaze implements Serializable {
         }
         return success;
     }
+
+    /**
+     * Checks if player has won the game.
+     * @param theX
+     * @param theY
+     * @return boolean
+     */
     private boolean atExit(final int theX, final int theY) {
         return theX == myRooms.length - 1 && theY == myRooms[theX].length - 1;
     }
 
+    /**
+     * Checks if the players move is valid and in bounds of the maze size.
+     * @param theRow
+     * @param theCol
+     * @return boolean
+     */
     private boolean validMove(final int theRow, final int theCol) {
         return theRow >= 0 && theRow < 4 && theCol >= 0 && theCol < 4 && !(myRooms[theRow][theCol].visited());
     }
 
+    /**
+     * ToString method that outputs the contents of the maze.
+     * @return String
+     */
     public String toString() {
         StringBuilder triviaMaze = new StringBuilder();
         for (int i = 0; i < myRooms.length; i++) {
